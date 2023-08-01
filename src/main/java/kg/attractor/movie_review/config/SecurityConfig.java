da767c20d1e06d4abdcea59de3e91afac6c2c05c
Aleksei Podstavin <gwinbleid86@gmail.com>
2023-08-01 21:10:11 +0600

    lesson 57

--- src/main/java/kg/attractor/movie_review/config/SecurityConfig.java
+++ src/main/java/kg/attractor/movie_review/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -55,12 +54,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
-                .formLogin(AbstractHttpConfigurer::disable)
-                .logout(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/movies/add/**")).hasAuthority("FULL")
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/add")).hasAuthority("FULL")
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/reviews/**")).fullyAuthenticated()
                         .anyRequest().permitAll()
                 );
