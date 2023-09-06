247468e958d0a825f91a6e0e7e2cefd4b7e4fed8
Aleksei Podstavin <gwinbleid86@gmail.com>
2023-09-06 17:45:54 +0600

    lesson 67

--- src/main/java/kg/attractor/movie_review/model/User.java
+++ src/main/java/kg/attractor/movie_review/model/User.java
@@ -3,6 +3,7 @@ package kg.attractor.movie_review.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Collection;
 import java.util.List;
 
 @Getter
@@ -24,4 +25,7 @@ public class User {
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "reviewer")
     private List<Review> reviews;
+
+    @ManyToMany(fetch = FetchType.EAGER, mappedBy = "users")
+    private Collection<Role> roles;
 }
